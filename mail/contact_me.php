5b82979ce5fef49a0d17e421fa0fab10702f7448
rparrar <roberto@rpi.cl>
2022-03-24 23:30:24 -0300

    agregando archivos

---
+++ mail/contact_me.php
@@ -0,0 +1,29 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "Error en envío de mensaje, por favor reintente";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'saniet70@hotmail.com'; 
+$email_subject = "Contacto desde tu web fullestucos:  $name";
+$email_body = "Haz recibido un mensaje desde tu web!\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$headers = "From: nocontestar@fullestucos.cl\n"; 
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+?>
+<script>
+	function r() { location.href="../mensajeok.html" } 
+	setTimeout ("r()", 1);
+</script>
